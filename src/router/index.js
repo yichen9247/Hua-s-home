7874cc9bdd4613052b081a902729036a2442dec9
yichen9247 <865252486@qq.com>
2025-01-31 14:54:23 +0800

    handsock 2.0.1（fix router）

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'home',
       component: HomePage,
-    }
-  ]
+  }, {
+      path: "/:catchAll(.*)",
+      component: () => HomePage,
+  }]
 });
 
 export default router;
