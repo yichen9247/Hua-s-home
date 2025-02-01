44336e576d57ad3ad17a9b3cbc890cb19b5204ba
yichen9247 <865252486@qq.com>
2025-02-01 21:30:30 +0800

    about layout update

--- src/model/UserInfo.vue
+++ src/model/UserInfo.vue
@@ -2,7 +2,7 @@
     <div class="model-user">
         <div class="user-contianer">
             <div class="avatar-box">
-                <img class="avatar-img" src="../assets/avatar.jpg" alt="Hua">
+                <img class="avatar-img" src="../assets/avatar.jpg" alt="Hua" draggable="false">
             </div>
             <p class="user-profile username">Hua</p>
             <p class="user-profile userdesc">一个无名小卒，擅长使用Java、Node等开发技术</p>
