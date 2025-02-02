f1fadca20d37084d65dc1201c783602be3b598fc
yichen9247 <865252486@qq.com>
2025-02-02 17:28:44 +0800

    content data update

--- src/layout/ProjectLayout.vue
+++ src/layout/ProjectLayout.vue
@@ -17,22 +17,6 @@
         name: 'Handsock',
         repo: 'https://github.com/yichen9247/HandSock',
         desc: '一款有趣的聊天应用，基于 Typescript，Mybatis-Plus，Springboot, Vue3 和 Socket.io，Redis 等技术开发'
-    }, {
-        name: 'Reomecho',
-        repo: 'https://github.com/yichen9247/ReomEcho',
-        desc: '一款基于Vue3编写的轻量级博客框架，旨在使用简洁的文档语言来编写博客文章，，基于 Typescript，Mybatis-Plus，Springboot, Vue3 和 Socket.io，Redis 等技术开发'
-    }, {
-        name: 'Randmusic',
-        repo: 'https://github.com/yichen9247/Randmusic',
-        desc: '一款基于网易云音乐嵌入型随机音乐播放器，使用PHP、Redis等技术进行编写'
-    }, {
-        name: 'Timextool',
-        repo: 'https://github.com/yichen9247/Timextool',
-        desc: '一个针对服务器运维开发的终端命令行工具，旨在让运维变得更加简单，提升运维及管理效率'
-    }, {
-        name: 'Windows11 React',
-        repo: 'https://github.com/yichen9247/Windows11-React',
-        desc: '一个基于React编写的高仿Windows11项目，不过已经很久没有维护了，预计会在未来进行重构'
     }]);
 </script>
 
--- src/layout/WebsiteLayout.vue
+++ src/layout/WebsiteLayout.vue
@@ -22,22 +22,6 @@
         link: 'https://doc.handsock.xiaokolomi.cn',
         desc: '项目HandSock的官方文档，不会搭建或者想要了解本项目的可以进入文档查看'
     }, {
-        name: 'Reomecho我的博客',
-        link: 'https://boke.xiaokolomi.cn/',
-        desc: '项目Reomecho的演示站点，同时也是我的个人博客，此站点将在未来进行重构'
-    }, {
-        name: 'Reomecho官方网站',
-        link: 'https://office.boke.xiaokolomi.cn',
-        desc: '项目Reomecho的官方网站，不会搭建或者想要了解本项目的可以进入网站查看'
-    }, {
-        name: '逐梦音乐工作站',
-        link: 'https://ai.music.xiaokolomi.cn',
-        desc: '一个聚合AI音乐的网站，在这里你可以找到另类的AI歌曲，发现音乐的不同唱法'
-    }, {
-        name: '头像生成工作站',
-        link: 'https://avatar.xiaokolomi.cn',
-        desc: '一个可以随机生成不同风格的头像网站，你可以随机生成不同形状和颜色的头像'
-    }, {
         name: 'SPlayer音乐网站',
         link: 'https://music.xiaokolomi.cn',
         desc: '基于开源项目SPlayer搭建的网易云在线音乐网站，界面美观且操作简单'
@@ -45,6 +29,10 @@
         name: 'Excalidraw在线绘图',
         link: 'https://board.xiaokolomi.cn',
         desc: '基于开源项目Excalidraw-Pro搭建的在线绘图网站，界面美观且操作简单'
+    }, {
+        name: '头像生成工作站',
+        link: 'https://avatar.xiaokolomi.cn',
+        desc: '一个可以随机生成不同风格的头像网站，你可以随机生成不同形状和颜色的头像'
     }]);
 </script>
 
