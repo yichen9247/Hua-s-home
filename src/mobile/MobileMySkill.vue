ef3ae4fb886f1c7c22c5188b4f8fb710634ca5b6
yichen9247 <865252486@qq.com>
2026-01-02 15:21:19 +0800

    something update

--- src/mobile/MobileMySkill.vue
+++ src/mobile/MobileMySkill.vue
@@ -8,9 +8,6 @@
             <div class="skill-category" v-for="(block, index) in config.mySkill.data" :key="index" :style="{ '--delay': index * 0.1 + 's' }">
                 <div class="category-header">
                     <div class="category-icon">
-                        <!-- <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
-                            <path d="M12 2L2 7V12C2 16.5 4.23 20.68 7.62 23.15L12 21L16.38 23.15C19.77 20.68 22 16.5 22 12V7L12 2Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        </svg> -->
                         <img class="icon" :src="block.icon" :alt="block.name">
                     </div>
                     <h3 class="category-name">{{ block.name }}</h3>
@@ -23,13 +20,13 @@
                         :key="skillIndex"
                         :style="{ '--item-delay': skillIndex * 0.03 + 's' }"
                     >
-                        <div class="skill-icon">
+                        <!-- <div class="skill-icon">
                             <div class="icon-dots">
                                 <span></span>
                                 <span></span>
                                 <span></span>
                             </div>
-                        </div>
+                        </div> -->
                         <span class="skill-name">{{ item }}</span>
                         <div class="skill-level">
                             <div class="level-bar">
